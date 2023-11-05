80943be1086ba2db90dd34af78dbfc5dbb24dc11
hhmy27 <13163369616@163.com>
2023-11-05 20:46:24 +0800

    refactor: 💡 update sample

--- src/parsing/sample.ts
+++ src/parsing/sample.ts
@@ -1,103 +1,91 @@
 export const SAMPLE_YAML = `
-# Demo resume adapted from https://github.com/jsonresume/resume-schema/blob/master/sample.resume.json
-
 basics:
-  name: Richard Hendriks
-  label: Programmer
-  email: richard.hendriks@mail.com
-  phone: (912) 555-4321
-  url: http://richardhendricks.example.com
+  name: 姓名
+  label: 求职岗位
+  email: youremail@gmail.com
+  phone: (+86) 136-8888-8888
+  url: www.yourblog.com
   summary: 
-    Richard hails from Tulsa. He has earned degrees from the 
-    University of Oklahoma and Stanford. (Go Sooners and Cardinal!) 
-    Before starting Pied Piper, he worked for Hooli as a part time 
-    software developer. While his work focuses on applied information theory, 
-    mostly optimizing lossless compression schema of both the length-limited 
-    and adaptive variants, his non-work interests range widely, everything 
-    from quantum computing to chaos theory. He could tell you about it, but 
-    THAT would NOT be a “length-limited” conversation!
-  location:
-    city: San Francisco
-    countryCode: US
+    熟练掌握全栈开发和敏捷开发方法，具备 10 年以上的软件开发经验。拥有卓越的领导能力和团队管理经验
+    ，擅长协调和激励团队以实现共同目标。在多个大型项目中担任核心开发者，成功交付高质量的软件解决方案。
+
   profiles:
     - network: github
-      url: github.com/richardhendricks
-    - network: linkedin
-      url: linkedin.com/richardhendricks
+      url: GitHub 地址
 work:
-  - name: Pied Piper
-    position: CEO/President
-    location: Palo Alto, CA
+  - name: FLAG 公司
+    position: 软件程序员
+    location: 上海，中国
     url: http://piedpiper.example.com
     summary: 
-      Pied Piper is a multi-platform technology based on a proprietary 
-      universal compression algorithm that has consistently fielded high
-      Weisman Scores™ that are not merely competitive, but approach the 
-      theoretical limit of lossless compression.
+      在 FLAG 科技公司工作期间，我担任软件工程师的角色，负责关键模块的开发和优化。
     startDate: 2013-12
-    endDate: 2014-12
+    endDate: 2014-11
     highlights: 
-      - Build an algorithm for artist to detect if their music was violating 
-        copy right infringement laws
-        
-      - Successfully won Techcrunch Disrupt
+      - 设计并实现数据库结构的优化方案，将查询时间减少50%，显著改善系统的响应速度。
+      - 根据业务场景扩展了线性规划求解器的核心组件，成功将复杂物流规划业务落地。
+      - 参与团队的性能优化工作，使用[技术]将系统的响应时间从5秒降低到2秒，提升了用户的体验。
 
-      - Optimized an algorithm that holds the current world record for 
-        Weisman Scores
-         
+projects: 
+  - name: Pied Piper 音频压缩算法
+    description: 一种全新的音频压缩算法，能够提供高效且无损的音频压缩解决方案，压缩率达{xx}%。
+    startDate: 2016-08
+    endDate: 2018-08
+    url: missdirection.example.com
+    highlights:
+      - 要点 1
+      
+      - 要点 2
+      
+      - 要点 3
+
+
+education:
+  - institution: 燕京大学
+    url: https://www.ou.edu/
+    area: 计算机技术 学士
+    # score: 'Score: 4.0'
+    startDate: 2014-06
+    # endDate: 2014-06
+    courses:
+      # - DB1101 - Basic SQL
+      # - CS2011 - Java Introduction
+
+awards:
+  - title: 与工作有关的证书
+    date: 2014-11
+    awarder: 颁发者
+    summary: 证书效果描述
+  - title: 你的博客
+    # date: 2014-11
+    awarder: 博客专注于讨论的领域
+    summary: 访问量等数据。
+
+skills:
+  - name: 编程语言
+    keywords: ["Java", "Python", "JavaScript"]
+    
+  - name: 数据库
+    keywords: ["MySQL", "MongoDB"]
+  - name: 网络通信
+    keywords: ["HTTP", "RPC"]
+
+    
 volunteer:
   - organization: CoderDojo
+  
     position: Teacher
     url: http://coderdojo.example.com/
     startDate: 2012-01
     endDate: 2013-01
     highlights:
       - Awarded 'Teacher of the Month'
-
-education:
-  - institution: University of Oklahoma
-    url: https://www.ou.edu/
-    area: Information Technology
-    score: 'Score: 4.0'
-    startDate: 2011-06
-    endDate: 2014-01
-    courses:
-      - DB1101 - Basic SQL
-      - CS2011 - Java Introduction
-
-awards:
-  - title: Digital Compression Pioneer Award
-    date: 2014-11
-    awarder: Techcrunch
-    summary: There is no spoon.
-
-publications:
-  - name: Video compression for 3d media
-    publisher: Hooli
-    releaseDate: 2014-10
-    url: http://en.wikipedia.org/wiki/Silicon_Valley_(TV_series)
-    summary: 
-      Innovative middle-out compression algorithm that changes 
-      the way we store data. 
-
-skills:
-  - name: Web Development
-    keywords: ["HTML", "CSS", "Javascript"]
-    
-  - name: Compression
-    keywords: ["Mpeg", "MP4", "GIF"]
-
-projects: 
-  - name: Miss Direction
-    description: A mapping engine that misguides you
-    startDate: 2016-08
-    endDate: 2016-08
-    url: missdirection.example.com
-    highlights:
-      - Won award at AIHacks 2016
-      
-      - Built by all women team of newbie programmers
-      
-      - Using modern technologies such as GoogleMaps, Chrome Extension 
-        and Javascript
-`;
+# publications:
+#   - name: Video compression for 3d media
+#     publisher: Hooli
+#     releaseDate: 2014-10
+#     url: http://en.wikipedia.org/wiki/Silicon_Valley_(TV_series)
+#     summary: 
+#       Innovative middle-out compression algorithm that changes 
+#       the way we store data. 
+`
